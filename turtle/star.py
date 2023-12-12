4498523243badb45f7c50151c0ee69a7c2a33e2e
NevroHelios <dasharka05@gmail.com>
2023-12-12 20:00:25 +0530

    getting-started

---
+++ turtle/star.py
@@ -0,0 +1,13 @@
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor('light green')
+wn.title('Turtle')
+
+skk = turtle.Turtle()
+
+for i in range(5):
+    skk.right(144)
+    skk.forward(144)
+
+turtle.done()
